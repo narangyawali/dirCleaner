195d52be74550989f4480b95353d9ffda036e16a
naran gyawali <nryng123@gmail.com>
2021-10-21 16:19:43 +0545

    rc file included

---
+++ trydump.py
@@ -0,0 +1,61 @@
+
+# import os
+
+# #gives where the script is located
+# print(__file__)
+
+
+# #gives from where the script was called
+# print(os.getcwd())
+
+# name = "hello"
+
+# print(name[len(name)-3:len(name)])
+
+# ram = "nara.yan"
+# print("-----------------")
+
+
+
+# music=["mp3", "mp4"]
+# docs=["txt","pdf"]
+# img=["png","jpg"]
+# ext=""
+# for i in range(3):
+# 	ext = ext + fileName[-3 +i]
+# print(ext)
+# if ext in music:
+#         print(f"ext {ext} goes in music")
+# elif ext in docs:
+#     	print(f"ext {ext} goes in music")
+# elif ext in img:
+#         print(f"ext {ext} goes in music")
+# else:
+#     print("no match found you can append it ")
+# import os
+
+# file = "file.txt"
+# source =""
+# dest ="fileholder"
+# #dest= os.path.join("/fileholder" ,file)
+
+# print(os.getcwd())
+# def move(file, source,dest):
+#     os.rename(os.path.join(source,file), os.path.join(dest,file)) 
+
+# move(file,source,dest)
+
+
+
+ext = input("extension of the file \n")
+if ext[0]=='.':
+    ext = ext[1:len(ext)]
+print(ext)
+
+
+{
+
+    "Music" : ["mp3", "mp4"],
+	"Docs" : ["txt", "pdf",'pptx'],
+	"Img" : ["png", "jpg"]
+}
